5e0fee2eed7de95fac2b1cf4ba16fa5cd7b2c57d
Paige Wiener <snowplushie@yahoo.com>
2019-03-06 05:49:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(hash)
   smallest_key = nil
   smallest_value = nil
-  hash.each do |key, value|
+  hash.collect do |key, value|
     if smallest_value == nil || value < smallest_value
       smallest_value = value
       smallest_key = key
